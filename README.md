679de3e416b3b797aca8ea62f841fa93eade2d7b
JJV <25530194@sun.ac.za>
2023-03-18 18:30:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# TestSuiteALAN
+
+## Cloning
+1. `git clone https://github.com/jjvraw/TestSuiteProcMan.git` into directory/folder which holds your 314 folder.
+2. Navigate to directory `cd TestSuiteProcMan`
+3. Run a script `python3 <filepath to proc_mngmnt>` - e.g. `python3 test.py 12345678-rw314/tut4/proc_mngmnt` 
+
+---
+TestCases: 
+1. Priority Schedulet 1 - Given Test Case
+2. Priority Scheduler 2 - Duplicate Process Names with Different Priorities
+3. Priority Scheduler 3 - Duplicate Resource Names
+4. Priority Scheduler 4 - Long Boy
+5. FCFS 1 - Given Test Case
+6. FCFS 2 - Longer Test Case
+---
---
+++ test.py
@@ -0,0 +1,55 @@
+import sys
+import difflib
+import subprocess
+
+def compare_files(file_path_1, file_path_2):
+    with open(file_path_1, 'r') as file_1, open(file_path_2, 'r') as file_2:
+        file_1_contents = file_1.read()
+        file_2_contents = file_2.read()
+        if file_1_contents == file_2_contents:
+            return True
+        else:
+            return False
+        
+
+def display_file_diff(file_path_1, file_path_2):
+    with open(file_path_1, 'r') as file_1, open(file_path_2, 'r') as file_2:
+        file_1_contents = file_1.readlines()
+        file_2_contents = file_2.readlines()
+
+    diff = difflib.unified_diff(file_1_contents, file_2_contents)
+
+    print(''.join(diff))
+
+def execute_command_in_directory(command, directory):
+    output = subprocess.run(command, shell=True, cwd=directory, capture_output=True, text=True)
+    return output
+
+if __name__ == '__main__':
+    fp = sys.argv[1]
+    arr = {chr(i): chr(i) for i in range(ord('A'), ord('F')+1)}
+    arr['A'] = "Priority Schedulet 1 - Given Test Case"
+    arr['B'] = "Priority Scheduler 2 - Duplicate Process Names with Different Priorities"
+    arr['C'] = "Priority Scheduler 3 - Duplicate Resource Names"
+    arr['D'] = "Priority Scheduler 4 - Long Boy"
+    arr['E'] = "FCFS 1 - Given Test Case"
+    arr['F'] = "FCFS 2 - Longer Test Case"
+
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
+    for i in arr:
+        execute_command_in_directory('make clean', fp)
+        execute_command_in_directory('make', fp)
+        if arr[i].startswith("FCFS") == False:
+            execute_command_in_directory('./schedule_processes ../../../TestSuiteProcman/TestIn/process' + i + '1.list ../../../TestSuiteProcman/TestIn/process' + i + '2.list 0 2', fp)
+        else: 
+            execute_command_in_directory('./schedule_processes ../../../TestSuiteProcman/TestIn/process' + i + '1.list ../../../TestSuiteProcman/TestIn/process' + i + '2.list 2 2', fp)
+
+        if compare_files(fp + "scheduler.log", "TestOut/" + i+ ".log"): 
+            print(f'{GREEN}[PASSED]{END} {arr[i]}')    
+        else: 
+            print(f'{RED}[FAILED]{END} {arr[i]}')
+            print(f'{RED}Differences:{END} \n')
+            display_file_diff(fp + "scheduler.log", "TestOut/A.log")
